b80ecf3838c20316378464039eb0c5184253bf8a
Ilma Hasanah <ilmahasanah1111@gmail.com>
2025-06-02 21:52:24 +0800

    Implementasi menu stack sederhana

---
+++ C030324021stack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+#define max 5
+int top = 0;
+
+int main (){
+    int pil; 
+    string isi;
+
+    stack :
+        cout << "Menu Utama\n1. Push\n1. Pop\nPilih :";
+        cin >> pil;
+    
+    if(pil == 1){
+        cout << "Push";
+    } else if(pil == 2){
+        cout << "Pop";
+    }
+}
